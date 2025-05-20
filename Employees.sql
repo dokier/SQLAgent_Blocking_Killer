e4b9c58cb0549041f69005e1a0779b91645d1dd2
Daniel Berber <dokier@hotmail.com>
2025-05-20 09:54:02 -0500

    Create Employees.sql

---
+++ Employees.sql
@@ -0,0 +1,26 @@
+USE [NSP_TEMP]
+GO
+
+/****** Object:  Table [dbo].[Employees]    Script Date: 5/20/2025 9:53:04 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employees](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[created_date] [datetime2](7) NULL,
+ CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Employees] ADD  CONSTRAINT [DF_Employees_created_date]  DEFAULT (getutcdate()) FOR [created_date]
+GO
+
+
